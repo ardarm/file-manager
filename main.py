71c3cbfd6b2e2471564b3e3b0e3e1fbf07723365
Bob Belderbos <bobbelderbos@gmail.com>
2021-05-31 13:19:58 +0200

    session code

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
 
 from arguments import get_args
-from operations import *
+from operations import (read_files, create_files,
+                        copy_files, move_file,
+                        remove_file)
 
 
 def main():
     """Main function"""
-
     args = get_args()
-    cat_arg = args.read
-    touch_arg = args.create
-    cp_arg = args.copy
+    files_to_read = args.read
+    files_to_create = args.create
+    files_to_copy = args.copy
     mv_arg = args.move
     rm_arg = args.remove
 
-    if cat_arg is not None:
-        files_to_read = cat_arg
-        read_files = [read_file(file) for file in files_to_read]
-    elif touch_arg is not None:
-        files_to_create = touch_arg
-        create_files = [create_file(file) for file in files_to_create]
-    elif cp_arg is not None:
-        if len(cp_arg) < 2:
-            print("\nCommand must have minimal 2 arguments")
-        else:
-            files_to_copy = cp_arg[:len(cp_arg) - 1]
-            copy_dest = cp_arg[len(cp_arg) - 1]
-            for file in files_to_copy:
-                copy_file(file, copy_dest)
+    if files_to_read is not None:
+        read_files(files_to_read)
+    elif files_to_create is not None:
+        create_files(files_to_create)
+    elif files_to_copy is not None:
+        copy_files(files_to_copy)
     elif mv_arg is not None:
         if len(mv_arg) < 2:
             print("\nMust have minimal 2 arguments!")
--- operations.py
+++ operations.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import os
 
 
-def read_file(filename):
+def _read_file(filename):
     try:
         with open(filename) as f:
             print(f.read())
@@ -15,7 +15,12 @@ def read_file(filename):
         print(f"\n{filename} is a directory, cannot be read!")
 
 
-def create_file(filename):
+def read_files(*files):
+    for file in files:
+        _read_file(file)
+
+
+def _create_file(filename):
     try:
         Path(filename).touch(exist_ok=False)
         print(f"{filename} created")
@@ -23,7 +28,12 @@ def create_file(filename):
         print(f"{filename} already exist!")
 
 
-def copy_file(filename, destination):
+def create_files(files):
+    for file in files:
+        _create_file(file)
+
+
+def _copy_file(filename, destination):
     try:
         shutil.copy(filename, destination)
         print(f"{filename} copied to {destination}")
@@ -33,6 +43,15 @@ def copy_file(filename, destination):
         print(f"\n{filename} is a directory, feature will be added soon!")
     except FileNotFoundError:
         print(f"{filename} doesn't exist")
+
+
+def copy_files(files):
+    if len(files) < 2:
+        raise ValueError(
+            "\nCommand must have minimal 2 arguments")
+    *filenames, destination = files
+    for filename in filenames:
+        _copy_file(filename, destination)
 
 
 def move_file(filename, destination):
