9199ca0635079895c5b46b5625a84bfb813330ad
ardarm <arisrooney.09@gmail.com>
2021-06-21 12:25:52 +0700

    embed operations function into interface

--- interface.py
+++ interface.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from tkinter import Frame, Button, Listbox
-
+from tkinter import Frame, Button, Listbox, StringVar, Entry
+import os
 
 class Interface():
     def __init__(self, window, operations):
         self.window = window
-        self.window.title("File Manager")
+        self.window.title("PDM File Manager")
         self.window.geometry("547x300")
 
         self.operations = operations
@@ -16,26 +16,105 @@ class Interface():
 
         self.button_new_folder = Button(self.frame_top,
                                         width=5,
-                                        text='New Folder')
+                                        text='New Folder',
+                                        command=lambda: self.handle_button_click(self.create_directory_trigger))
         self.button_new_folder.grid(row=0, column=1, sticky='w', padx=5)
-        self.button_new_file = Button(self.frame_top, width=5, text='New File')
+        self.button_new_file = Button(self.frame_top, width=5, text='New File', command=lambda: self.handle_button_click(self.create_file_trigger))
         self.button_new_file.grid(row=0, column=2, sticky='w', padx=5)
-        self.button_rename = Button(self.frame_top, width=5, text='Rename')
+        self.button_rename = Button(self.frame_top, width=5, text='Rename', command=lambda: self.handle_button_click(self.rename_trigger))
         self.button_rename.grid(row=0, column=3, sticky='w', padx=5)
-        self.button_delete = Button(self.frame_top, width=5, text='Delete')
+        self.button_delete = Button(self.frame_top, width=5, text='Delete', command=lambda: self.handle_button_click(self.delete_trigger))
         self.button_delete.grid(row=0, column=4, sticky='w', padx=5)
-        self.button_copy = Button(self.frame_top, width=5, text='Copy')
+        self.button_copy = Button(self.frame_top, width=5, text='Copy', command=lambda: self.handle_button_click(self.copy_trigger))
         self.button_copy.grid(row=0, column=5, sticky='w', padx=5)
-        self.button_cut = Button(self.frame_top, width=5, text='Cut')
+        self.button_cut = Button(self.frame_top, width=5, text='Move', command=lambda: self.handle_button_click(self.move_trigger))
         self.button_cut.grid(row=0, column=6, sticky='w', padx=5)
 
         self.frame_middle = Frame(self.window)
         self.frame_middle.grid(columnspan=8)
 
         self.left_listbox = Listbox(self.frame_middle, width=25)
-        self.left_listbox.bind('<Double-Button-1>')
+        self.left_listbox.bind('<Double-Button-1>', lambda _: self.handle_item_click(self.left_listbox))
         self.left_listbox.grid(row=2, column=0, sticky='w')
 
         self.right_listbox = Listbox(self.frame_middle, width=25)
-        self.right_listbox.bind('<Double-Button-1>')
+        self.right_listbox.bind('<Double-Button-1>', lambda _: self.handle_item_click(self.right_listbox))
         self.right_listbox.grid(row=2, column=4, sticky='e')
+
+    def handle_button_click(self, trigger):
+        if len(self.left_listbox.curselection()) > 0:
+            trigger(self.left_listbox, self.right_listbox)
+        elif len(self.right_listbox.curselection()) > 0:
+            trigger(self.right_listbox, self.right_listbox)
+
+    def handle_item_click(self, listbox):
+        self.listbox = listbox
+        item = self.operations.get_selected(listbox)
+        print(item)
+        self.operations.get_directory_contents(listbox)
+        self.operations.traverse_directory(item)
+        os.chdir(self.operations.current_directory)
+        self.operations.get_directory_contents(listbox)
+
+    def get_selected_item(self, listbox):
+        idx = listbox.curselection()[0]
+        if listbox.get(idx) != '..':
+            item = listbox.get(idx)
+        return item
+
+    def del_edit_spot(self):
+        self.edit_spot.destroy()
+        self.button_ok.destroy()
+        self.button_cancel.destroy()
+
+    def refresh(self):
+        self.operations.get_directory_contents(self.left_listbox)
+        self.operations.get_directory_contents(self.right_listbox)
+
+    def create_edit_spot(self, *args):
+        item = None
+        v = StringVar()
+        bottom_frame = Frame(self.window).grid(columnspan=8, sticky='we')
+        self.edit_spot = Entry(bottom_frame, textvariable=v, )
+        if len(args) == 3:
+            item = args[2]
+            self.edit_spot.insert(0, item)
+        listbox, trigger = args[0], args[1]
+        self.edit_spot.grid(row=2, column=2, columnspan=4, padx=50, sticky='e')
+
+        self.button_ok = Button(bottom_frame, text='ok',command=lambda: trigger(listbox, self.edit_spot.get(), self.del_edit_spot, self.refresh, item))
+        self.button_ok.grid(row=2, column=4, sticky='e', columnspan=2)
+        self.button_cancel = Button(bottom_frame, text='cancel', command=lambda: self.del_edit_spot())
+        self.button_cancel.grid(row=2, column=6, sticky='w', columnspan=2)
+
+    def create_file_trigger(self, *args):
+        listbox = args[0]
+        self.create_edit_spot(listbox, self.operations.create_file)
+
+    def create_directory_trigger(self, *args):
+        listbox = args[0]
+        self.create_edit_spot(listbox, self.operations.create_directory)
+
+    def rename_trigger(self, *args):
+        listbox = args[0]
+        item = self.get_selected_item(listbox)
+        self.create_edit_spot(listbox, self.operations.rename, item)
+
+    def delete_trigger(self, *args):
+        listbox = args[0]
+        item = self.get_selected_item(listbox)
+        self.operations.delete(item, listbox, self.refresh)
+
+    def copy_trigger(self, *args):
+        source, destination = args[0], args[1]
+        item = self.get_selected_item(source)
+        self.operations.current_directory = self.operations.get_listbox()
+        destination = self.operations.current_directory
+        self.operations.copy(item, destination, self.refresh)
+
+    def move_trigger(self, *args):
+        source, destination = args[0], args[1]
+        item = self.get_selected_item(source)
+        self.operations.current_directory = self.operations.get_listbox()
+        destination = self.operations.current_directory
+        self.operations.move(item, destination, self.refresh)
--- operations.py
+++ operations.py
@@ -2,13 +2,43 @@
 
 import os
 from tkinter import END
+from pathlib import Path
+from shutil import SameFileError, copytree, rmtree, copy, move
 
 
 class Operations():
     def __init__(self):
         self.current_directory = os.getcwd()
-        self.left_listbox = os.getcwd()
-        self.right_listbox = os.getcwd()
+        self.left_listbox_path = os.getcwd()
+        self.right_listbox_path = os.getcwd()
+
+    def get_listbox(self, **kwargs):
+        if kwargs: 
+            self.listbox = kwargs['listbox']
+        if str(self.listbox)[-1] == "2":
+            self.current_directory = self.right_listbox_path
+            return self.right_listbox_path
+        else:
+            self.current_directory = self.left_listbox_path
+            return self.left_listbox_path
+
+    def get_selected(self, listbox):
+        print(listbox.curselection())
+        i = listbox.curselection()[0]
+        print(i)    
+        self.selected_item = listbox.get(i)
+        return self.selected_item
+
+    def update_listbox_path(self, new_path):
+        print(self.listbox)
+        if str(self.listbox)[-1] == "2":
+            self.right_listbox_path = new_path
+            self.current_directory = self.right_listbox_path
+            return self.right_listbox_path
+        else:
+            self.left_listbox_path = new_path
+            self.current_directory = self.left_listbox_path
+            return self.left_listbox_path
 
     def get_directory_contents(self, listbox):
         self.listbox = listbox
@@ -23,10 +53,115 @@ class Operations():
                 self.listbox.insert(END, file)
             break
 
-    def get_listbox(self):
-        if str(self.listbox)[-1] == "2":
-            self.current_directory = self.right_listbox
-            return self.right_listbox
+    def traverse_directory(self, item):
+        if item == "..":
+            new_path = os.path.join(self.current_directory, item)
+            new_path = os.path.abspath(new_path)
+            self.current_directory = new_path
+            self.update_listbox_path(self.current_directory)
+        elif os.path.isdir(item):
+            new_path = os.path.join(self.current_directory, item)
+            self.current_directory = new_path
+            self.update_listbox_path(self.current_directory)
+
+    def _create_file(self, filename):
+        try:
+            Path(filename).touch(exist_ok=False)
+            return True
+        except FileExistsError:
+            print(f'File {filename} already exist!')
+
+    def create_file(self, *args):
+        listbox, filename, del_edit_spot, refresh, item = args
+        response = self._create_file(filename)
+        if response == True:
+            del_edit_spot()
+            refresh()
+
+    def _create_directory(self, dirname):
+        try:
+            os.mkdir(dirname)
+            return True
+        except FileExistsError:
+            print(f'Directory {dirname} already exist!')
+
+    def create_directory(self, *args):
+        listbox, dirname, del_edit_spot, refresh, item = args
+        response = self._create_directory(dirname)
+        if response == True:
+            del_edit_spot()
+            refresh()
+
+    def _rename(self, current_name, new_name):
+        if os.path.exists(new_name) and os.path.isfile(new_name):
+            return print(f"file {new_name} exist!")
+        elif os.path.exists(new_name) and os.path.isdir(new_name):
+            return print(f"directory {new_name} exist!")
         else:
-            self.current_directory = self.left_listbox
-            return self.left_listbox
+            os.rename(current_name, new_name)
+            return True
+        
+    def rename(self, *args):
+        listbox, new_name, del_edit_spot, refresh, current_name = args
+        self.get_directory_contents(listbox)
+        #self.current_directory = self.get_listbox()
+        #os.chdir(self.current_directory)
+        response = self._rename(current_name, new_name)
+        if response == True:
+            del_edit_spot()
+            refresh()
+
+    def _delete(self, item):
+        if os.path.isfile(item):
+            os.remove(item)
+        elif os.path.isdir(item):
+            rmtree(item)
+        else:
+            print("unknown type")
+
+    def delete(self, item, listbox, refresh):
+        #self.get_directory_contents(listbox)
+        #self.current_directory = self.get_listbox()
+        #os.chdir(self.current_directory)
+        print(self.current_directory)
+        self._delete(item)
+        refresh()
+
+    def _copy_file(self, file, destination):
+        try:
+            copy(file, destination)
+            print(f"{file} copied to {destination}")
+        except SameFileError:
+            print(f"\n{file} already exist!")
+
+    def _copy_directory(self, directory, destination):
+        copytree(directory, destination)
+
+    def copy(self, item, destination, refresh):
+        if destination == self.left_listbox_path:
+            item_path = os.path.join(self.right_listbox_path, item)
+            #os.chdir(self.right_listbox_path)
+        else:
+            item_path = os.path.join(self.left_listbox_path, item)
+            #os.chdir(self.left_listbox_path)
+        if os.path.isfile(item_path):
+            self._copy_file(item_path, destination)
+        elif os.path.isdir(item_path):
+            self._copy_directory(item_path, destination)
+        else:
+            print("unknown type")
+        refresh()
+
+    def _move(self, item, destination):
+        move(item, destination)
+        print(f"{item} moved to {destination}")
+
+    def move(self, item, destination, refresh):
+        if destination == self.left_listbox_path:
+            item_path = os.path.join(self.right_listbox_path, item)
+            #os.chdir(self.right_listbox_path)
+        else:
+            item_path = os.path.join(self.left_listbox_path, item)
+            #os.chdir(self.left_listbox_path)
+        self._move(item_path, destination)
+        refresh()
